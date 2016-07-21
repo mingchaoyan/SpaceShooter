7d4c083ddfb9f0f1bacb1929ca77a9cc52557c59
mingchaoyan <mingchaoyan@gmail.com>
2016-07-22 00:10:49 +0800

    制作子弹预制体

---
+++ Assets/Scripts/BoltMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoltMove : MonoBehaviour {
+	public float speed =40.0f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+	
+	}
+}
