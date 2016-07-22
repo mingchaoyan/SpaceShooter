0f5a66df49ff550def705fecdcbdf32e9cbf52d6
mingchaoyan <mingchaoyan@gmail.com>
2016-07-22 17:15:42 +0800

    销毁特效

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+	public float lifetime =2.0f;
+
+	void Start () {
+		Destroy(gameObject, lifetime);	
+	}
+}
