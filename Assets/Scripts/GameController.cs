7aba371918b39956c7758e74f02f94beaf03504a
mingchaoyan <mingchaoyan@gmail.com>
2016-07-22 16:54:11 +0800

    小行星随机产生

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public Vector3 spawnPosition=Vector3.zero;
+	private Quaternion spawnRotation;
+
+	void SpawnWaves() {
+		spawnPosition.x = Random.Range(-spawnValues.x, spawnValues.x);
+		spawnPosition.z = spawnValues.z;
+		spawnRotation = Quaternion.identity;
+		Instantiate(hazard, spawnPosition, spawnRotation);
+	}
+
+	// Use this for initialization
+	void Start () {
+		SpawnWaves();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
