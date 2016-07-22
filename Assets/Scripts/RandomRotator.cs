944d344118cc4049bc9a5ea8e2f3a00ad2c45341
mingchaoyan <mingchaoyan@gmail.com>
2016-07-22 16:24:07 +0800

    碰撞销毁逻辑

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	public float tumble = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere*tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
